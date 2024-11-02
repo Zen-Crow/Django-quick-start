a643ac305f4cdde1df86ecdd322093672c407f31
git-lab-user1 <gurovam184@gmail.com>
2024-11-02 10:57:10 +0300

    init

--- setup_env.ps1
+++ setup_env.ps1
@@ -33,7 +33,6 @@ django-cors-headers==4.6.0
 djangorestframework==3.15.2
 djangorestframework-simplejwt==5.3.1
 psycopg2-binary==2.9.10
-
 environs==11.0.0
 gunicorn==23.0.0 
 "@ | Out-File -FilePath $requirementsPath -Encoding utf8
